2b2f964dc8f318a4fd50694696c598fdb3903a01
Thijs Berrevoets <74351651+Thijsy1999@users.noreply.github.com>
2023-11-01 15:12:14 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,109 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+
+        }
+        // if you want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 115);
+
+    // remove toggle icon and navbar when click links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+const dynamicText = document.querySelector("h3 span");
+const words = ["User Experience Designer", "User Interface Designer", "Frontend Developer"];
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 150);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 50);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+typeEffect();
+
+//sendMail Function Using emailJS
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        number: document.getElementById("number").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_vgg7nnc";
+    const templateID = "template_9rujtyl";
+
+    if (document.getElementById("name").value == "" || document.getElementById("email").value == ""
+    || document.getElementById("number").value == "" || document.getElementById("subject").value == ""
+    || document.getElementById("message").value == "") {
+        alert("Ensure you input a value in all the fields above!");
+    }
+    else { emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("number").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Mail send succesfully!");
+        })
+        .catch((err) => console.log(err));
+    }
+}
+
+// footer get id=year -> insert realtime year data
+document.getElementById("year").innerHTML = new Date().getFullYear();
